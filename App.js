3fa0bbbe833d150cc65c28a588609fe317dc3c7f
alie <53941410+ajallow-hub@users.noreply.github.com>
2021-01-10 00:19:45 +0000

    App.js Added

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FormValidation from "./FormValidation";
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+       <FormValidation /> 
+    
+      </div>
+    );
+  }
+}
+
+export default App;
